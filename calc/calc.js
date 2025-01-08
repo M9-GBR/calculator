fd07c5e85aee251b6291597d2e16a9dd942713ce
M9-GBR <150633267+M9-GBR@users.noreply.github.com>
2025-01-08 02:44:26 +0100

    Add files via upload

--- calc/calc.js
+++ calc/calc.js
@@ -1,4 +1,7 @@
 import keys from './keys.json' with {type: 'json'}
+import style from './calc.css' with {type: 'css'}
+
+document.adoptedStyleSheets.push(style)
 
 export default class Calculator {
     data = {
@@ -27,13 +30,19 @@ export default class Calculator {
         swithCurrentVals() {
             this.nextInput = !this.nextInput
         },
+        switchToAnswerVal() {
+            this.val1 = this.answerVal
+            this.val2 = ''
+            this.operand = ''
+            this.answerVal = ''
+            this.nextInput = false
+        },
         evalData() {
             return String(eval(`${Number(this.val1)}${this.operand}${Number(this.val2)}`))
         }
     }
 
     constructor(parentElem, id = '') {
-        window.calc = this
         this.parentElem = parentElem
         this.id = id
         this.makeBody()
@@ -100,6 +109,10 @@ export default class Calculator {
     }
 
     inputNum(char) {
+        if (this.data.answerVal != '') {
+            this.data.reset()
+        }
+
         char = String(char)
 
         let number = this.data.getCurrentNumber(), regex = /^[0]{1,}/
@@ -117,6 +130,10 @@ export default class Calculator {
     }
 
     inputDecimal() {
+        if (this.data.answerVal != '') {
+            this.data.reset()
+        }
+
         let number = this.data.getCurrentNumber()
 
         if (number == '') {
@@ -128,6 +145,10 @@ export default class Calculator {
     }
 
     negate() {
+        if (this.data.answerVal != '') {
+            this.data.switchToAnswerVal()
+        }
+
         let number = this.data.getCurrentNumber()
 
         if (number != '' && number != 0)
@@ -144,6 +165,10 @@ export default class Calculator {
     }
 
     delChars() {
+        if (this.data.answerVal != '') {
+            this.data.switchToAnswerVal()
+        }
+
         let number = this.data.getCurrentNumber(),
             arr = number.split('')
 
@@ -173,16 +198,28 @@ export default class Calculator {
     }
 
     addOperand(type) {
+        if (this.data.answerVal != '') {
+            this.data.switchToAnswerVal()
+        }
+
         this.displayBlink()
 
         if (this.data.operand == '') {
             this.data.swithCurrentVals()
         }
 
+        if (this.data.val1 != '' && this.data.val2 != '' && this.data.operand != '') {
+            this.evaluateCalc()
+        }
+
         this.data.operand = type
     }
 
     root() {
+        if (this.data.answerVal != '') {
+            this.data.switchToAnswerVal()
+        }
+
         let number = this.data.getCurrentNumber()
 
         if (number != '') {
@@ -199,12 +236,14 @@ export default class Calculator {
 
     evaluateCalc() {
         if (this.data.operand != '') {
+            if (this.data.answerVal != '') {
+                this.data.val1 = this.data.answerVal
+            }
+
             this.displayBlink()
 
             this.data.answerVal = this.data.evalData()
             this.displayNumber(this.data.answerVal)
-
-            this.data.reset()
         }
     }
 }
